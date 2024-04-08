e579c3f03eff30412184aa35dff991e73fb0e56b
Ton Ha <ton@tdha.co>
2024-04-08 13:58:59 +1000

    Lab part 1 done. Added about, about-index.

--- peeps/views.py
+++ peeps/views.py
@@ -1,4 +1,17 @@
 from django.shortcuts import render
 
+peeps = [
+  {'name': 'Jim', 'title': 'Lead instructor', 'nickname': 'Jimbo', 'age': 50},
+  {'name': 'Laura', 'title': 'Associate instructor', 'nickname': 'Pez', 'age': 32},
+]
+
 def home(request):
     return render(request, 'home.html')
+
+def about(request):
+    return render(request, 'about.html')
+
+def peeps_index(request):
+    return render(request, 'peeps/index.html', {
+        'peeps': peeps
+    })
