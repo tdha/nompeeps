089d2d245a8fcecf8f97b19fa763153d834c795d
Ton Ha <ton@tdha.co>
2024-04-09 15:02:21 +1000

    Added CRUD delete.

--- peeps/models.py
+++ peeps/models.py
@@ -1,3 +1,17 @@
-from django.db import models
+from django.db import models # database name is nompeeps
+from django.urls import reverse
 
-# Create your models here.
+class Nompeep(models.Model):
+    name = models.CharField(max_length=128)
+    date = models.DateField()
+    event = models.CharField(max_length=256, blank=True)
+    notes = models.TextField(max_length=512, blank=True)
+
+    def get_absolute_url(self):
+        return reverse('detail', kwargs={ 'peeps_id': self.id })
+
+    def __str__(self):
+        if self.event:
+            return f"Met {self.name} at {self.event} on {self.date}."
+        else:
+            return f"Met {self.name} on {self.date}."
--- peeps/views.py
+++ peeps/views.py
@@ -1,9 +1,6 @@
 from django.shortcuts import render
-
-peeps = [
-  {'name': 'Jim', 'title': 'Lead instructor', 'nickname': 'Jimbo', 'age': 50},
-  {'name': 'Laura', 'title': 'Associate instructor', 'nickname': 'Pez', 'age': 32},
-]
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from .models import Nompeep
 
 def home(request):
     return render(request, 'home.html')
@@ -12,6 +9,25 @@ def about(request):
     return render(request, 'about.html')
 
 def peeps_index(request):
+    peeps = Nompeep.objects.all()
     return render(request, 'peeps/index.html', {
         'peeps': peeps
     })
+
+def peeps_detail(request, peeps_id):
+    peeps = Nompeep.objects.get(id=peeps_id)
+    return render(request, 'peeps/detail.html', {
+        'peeps': peeps
+    })
+
+class NompeepCreate(CreateView):
+    model = Nompeep
+    fields = '__all__'
+
+class NompeepUpdate(UpdateView):
+    model = Nompeep
+    fields = '__all__'
+
+class NompeepDelete(DeleteView):
+    model = Nompeep
+    success_url = '/peeps/'
